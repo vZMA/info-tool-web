fe78269927e256f0c67b4dad09bc23cb8409638e
Tom Coombs <tom@cube74.com>
2024-10-20 15:02:47 -0400

    vnas updates

--- Features/VnasData/ScheduledJobs/FetchAndCacheVnasData.cs
+++ Features/VnasData/ScheduledJobs/FetchAndCacheVnasData.cs
@@ -1,58 +1,17 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-using Microsoft.Extensions.Caching.Memory;
+﻿using Coravel.Invocable;
 using Microsoft.Extensions.Options;
-using ZmaReference.Features.VnasData.Models;
+using ZmaReference.Features.Charts.ScheduledJobs;
+using ZmaReference.Features.Charts.Services;
+using ZmaReference.Features.VnasData.Services;
 
-namespace ZmaReference.Features.VnasData.Services;
+namespace ZmaReference.Features.VnasData.ScheduledJobs;
 
-public class CachedVnasDataService(IMemoryCache cache, IHttpClientFactory httpClientFactory, IOptionsMonitor<AppSettings> appSettings, ILogger<CachedVnasDataService> logger)
+public class FetchAndCacheVnasData(ILogger<FetchAndCacheCharts> logger, CachedVnasDataService vnasDataService) : IInvocable
 {
-    private readonly JsonSerializerOptions _jsonOptions = new()
+    public async Task Invoke()
     {
-        Converters = { new JsonStringEnumConverter() }
-    };
-
-    public async Task<IEnumerable<FacilityExtended>> GetArtccFacilities(string artccId, CancellationToken c = default)
-    {
-        if (cache.TryGetValue<IEnumerable<FacilityExtended>>(MakeFacilityCacheKey(artccId), out var cached))
-        {
-            logger.LogInformation("Found cached VNAS Facilities data for {artcc}", artccId);
-            return cached ?? Enumerable.Empty<FacilityExtended>();
-        }
-        
-        var jsonRoot = await GetJsonRoot(artccId, c);
-        if (jsonRoot is null)
-        {
-            return Enumerable.Empty<FacilityExtended>();
-        }
-        
-        var videoMapDict = jsonRoot.VideoMaps.ToDictionary(m => m.Id);
-        var returnFacilities = new List<FacilityExtended>();
-        var queue = new Queue<Facility>();
-        queue.Enqueue(jsonRoot.Facility);
-
-        while (queue.Count > 0)
-        {
-            var facility = queue.Dequeue();
-            var maps = facility.StarsConfiguration?.VideoMapIds.Select(m => videoMapDict.GetValueOrDefault(m)).Where(m => m is not null);
-            returnFacilities.Add(new FacilityExtended(facility, (maps ?? [])!));
-            facility.ChildFacilities.ForEach(child => queue.Enqueue(child));
-        }
-
-        var expiration = DateTimeOffset.UtcNow.AddSeconds(appSettings.CurrentValue.CacheTtls.VnasData);
-        cache.Set(MakeFacilityCacheKey(artccId), returnFacilities, expiration);
-        return returnFacilities;
+        // This method forces service to fetch and cache, and we can discard value
+        logger.LogInformation("Fetching all VNAS Data for ZMA");
+        await vnasDataService.ForceCache("ZMA");
     }
-
-    private async Task<VnasApiRoot?> GetJsonRoot(string artccId, CancellationToken c = default)
-    {
-        var httpClient = httpClientFactory.CreateClient();
-        var jsonRoot = await httpClient.GetFromJsonAsync<VnasApiRoot>($"{appSettings.CurrentValue.Urls.VnasApiEndpoint}/artccs/{artccId.ToUpper()}", _jsonOptions, c);
-        return jsonRoot;
-    }
-
-    public Task ForceCache(string artccId, CancellationToken c = default) => GetArtccFacilities(artccId, c);
-    
-    private static string MakeFacilityCacheKey(string id) => $"VnasDataFacility:{id}";
 }
