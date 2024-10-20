3698d0fc078ff3fd3f339531bb154c7fafc19415
Tom Coombs <tom@cube74.com>
2024-10-20 14:38:51 -0400

    added facility extensions

---
+++ Features/VnasData/Models/FacilityExtended.cs
@@ -0,0 +1,7 @@
+﻿namespace ZmaReference.Features.VnasData.Models;
+
+public class FacilityExtended(Facility facility, IEnumerable<VideoMap> videoMaps)
+{
+    public Facility Facility { get; set; } = facility;
+    public List<VideoMap> VideoMaps { get; set; } = videoMaps.ToList();
+}
